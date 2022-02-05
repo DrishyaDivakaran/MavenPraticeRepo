887acc9b03f4f3c40427b65ecafeca87739ba102
DrishyaDivakaran <drishyadivakaran000@gmail.com>
2022-02-05 17:00:13 +0530

    my first maven commit

---
+++ src/test/java/com/crm/Trial2Test/CreateContactTest.java
@@ -0,0 +1,19 @@
+package com.crm.Trial2Test;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public  void craeteContactTest()
+	{
+		System.out.println("execute create contact test");
+	}
+	
+	@Test
+	public void modiContactTest()
+	{
+		System.out.println("excute modification contact test");
+	}
+
+
+}
---
+++ src/test/java/com/crm/TrialTest/CreateOrganiztionTest.java
@@ -0,0 +1,19 @@
+package com.crm.TrialTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganiztionTest {
+	@Test
+	public void createOrganization()
+	{
+		System.out.println("excute create contact");
+	}
+	
+	@Test
+	public void modifyOrganization()
+	{
+		System.out.println("modify and excute");
+	}
+
+
+}
